b9ce7ddd54374aa56578b9ba2b8a467ddedea690
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2024-12-02 22:28:11 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Carousel from "./components/Carousel";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Navbar />
+        <Carousel />
+      </header>
+    </div>
+  );
+}
+
+export default App;
