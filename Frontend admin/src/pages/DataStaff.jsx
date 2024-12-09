128c37b0be44a6737e16e5acab5df1b8fb01d5d3
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2024-12-09 21:45:38 +0700

    update

--- Frontend admin/src/pages/DataStaff.jsx
+++ Frontend admin/src/pages/DataStaff.jsx
@@ -1,10 +1,119 @@
 import AuthenticatedLayout from "../components/AuthenticatedLayout";
+import DeleteButton from "../components/DeleteInfoPublik";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
 
 function DataStaff() {
+  const [informasi, setInformasiPublik] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const handleDeleteSuccess = () => {
+    console.log(
+      "Data berhasil dihapus, lakukan refresh data atau tindakan lain."
+    );
+    // Anda bisa melakukan refresh data atau update state di sini
+  };
+
+  const url = "http://localhost:8000/api/staff";
+
+  // Fetch data ketika komponen pertama kali di-render
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        // Mengambil data dari response.data.data
+        if (response.data.status === "success") {
+          setInformasiPublik(response.data.data); // Menyimpan data staff ke state
+        }
+        setLoading(false); // Menandakan bahwa loading sudah selesai
+      })
+      .catch((error) => {
+        setError(error.message); // Menangani error
+        setLoading(false); // Menandakan bahwa loading sudah selesai
+      });
+  }, []); // Dependency array kosong, artinya hanya dijalankan sekali saat komponen mount
+
+  if (loading) return <Loading></Loading>; // Tampilkan loading jika data sedang diambil
+  if (error) return <p>Error: {error}</p>; // Tampilkan error jika ada
+
   return (
     <div className="lg:grid grid-cols-6">
-      <AuthenticatedLayout />
-      <h1>ini data staff</h1>
+      <div>
+        <AuthenticatedLayout />
+      </div>
+
+      <div className="overflow-x-auto col-span-5 m-10">
+        <h1 className="text-4xl font-bold pb-5">Data Staff</h1>
+
+        {/* button tambah */}
+        <div className="py-10">
+          <a
+            className="inline-flex items-center gap-2 rounded border border-indigo-600 px-8 py-3 text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
+            href="/staff/tambah">
+            <span className="text-sm font-medium"> Tambah data </span>
+
+            <svg
+              className="size-5 rtl:rotate-180"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              />
+            </svg>
+          </a>
+        </div>
+        {/* button tambah */}
+
+        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+          <thead className="ltr:text-left rtl:text-right">
+            <tr>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Nama
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Jabatan
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Foto
+              </th>
+              <th></th>
+            </tr>
+          </thead>
+
+          <tbody className="divide-y divide-gray-200 text-center">
+            {informasi.map((member) => (
+              <tr key={member.id}>
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  {member.nama}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  {member.jabatan}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  <img
+                    src={`http://localhost:8000${member.foto}`}
+                    alt="foto staff"
+                    className="h-20 w-20 object-cover rounded-full mx-auto"
+                  />
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-red-600">
+                  <DeleteButton
+                    id={member.id}
+                    onSuccess={handleDeleteSuccess}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- Frontend/src/Pages/Informasipublik.jsx
+++ Frontend/src/Pages/Informasipublik.jsx
@@ -1,63 +1,102 @@
+import DeleteButton from "../components/DeleteInfoPublik";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+
 function Informasipublik() {
-    return (
-      <>
-        <div className="overflow-x-auto py-20 px-20 h-screen">
-          <h2 className="text-center text-2xl font-bold py-10 pb-20">
-            Informasi Publik
-          </h2>
-          <table className="table">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>Informasi</th>
-                <th>Judul</th>
-                <th>Dibuat</th>
-                <th>Tgl Dokumen</th>
-                <th>File</th>
-                <th>File</th>
-                <th>Ukuran</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* row 1 */}
-              <tr>
-                <th>Tersedia setiap saat</th>
-                <td>RTP BPKD Tahun 2023</td>
-                <td>Rencana Tindak Pengendalian (RTP) BPKD Tahun 2023</td>
-                <td>01-02-2024</td>
-                <td>03-07-2023</td>
-                <td>url</td>
-                <td>0 Bytes</td>
-                <td>Link</td>
-              </tr>
-              {/* row 2 */}
-              <tr>
-                <th>Tersedia setiap saat</th>
-                <td>RTP BPKD Tahun 2023</td>
-                <td>Rencana Tindak Pengendalian (RTP) BPKD Tahun 2023</td>
-                <td>01-02-2024</td>
-                <td>03-07-2023</td>
-                <td>url</td>
-                <td>0 Bytes</td>
-                <td>Link</td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <th>Tersedia setiap saat</th>
-                <td>RTP BPKD Tahun 2023</td>
-                <td>Rencana Tindak Pengendalian (RTP) BPKD Tahun 2023</td>
-                <td>01-02-2024</td>
-                <td>03-07-2023</td>
-                <td>url</td>
-                <td>0 Bytes</td>
-                <td>Link</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </>
+  const [informasi, setInformasiPublik] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const handleDeleteSuccess = () => {
+    console.log(
+      "Data berhasil dihapus, lakukan refresh data atau tindakan lain."
     );
+    // Anda bisa melakukan refresh data atau update state di sini
+  };
+
+  const url = "http://localhost:8000/api/informasi-publik";
+
+  // Fetch data ketika komponen pertama kali di-render
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        // Mengambil data dari response.data.data
+        if (response.data.status === "success") {
+          setInformasiPublik(response.data.data); // Menyimpan data staff ke state
+        }
+        setLoading(false); // Menandakan bahwa loading sudah selesai
+      })
+      .catch((error) => {
+        setError(error.message); // Menangani error
+        setLoading(false); // Menandakan bahwa loading sudah selesai
+      });
+  }, []); // Dependency array kosong, artinya hanya dijalankan sekali saat komponen mount
+
+  if (loading) return <Loading></Loading>; // Tampilkan loading jika data sedang diambil
+  if (error) return <p>Error: {error}</p>; // Tampilkan error jika ada
+
+  return (
+    <>
+      <div className="overflow-x-auto py-20 px-20 h-screen">
+        <h2 className="text-center text-2xl font-bold py-10 pb-20">
+          Informasi Publik
+        </h2>
+        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+          <thead className="ltr:text-left rtl:text-right">
+            <tr>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Informasi
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Judul
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Dibuat
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Tanggal Dokumen
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Ukuran
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                File
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className="divide-y divide-gray-200 text-center">
+            {informasi.map((member) => (
+              <tr key={member.id}>
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  {member.informasi}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  {member.judul}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  {member.created_at}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  {member.dibuat}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  {member.ukuran}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  <a href={member.url} target="_blank">
+                    Download
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
 
 export default Informasipublik;
